42a2961d17d36696996aac6708ca4cb5009b03cd
Anson Ming <128398768+AnsonMing@users.noreply.github.com>
2024-03-19 17:47:48 +0800

    update

--- code/main.py
+++ code/main.py
@@ -10,10 +10,10 @@ app = Flask(__name__)
 def check_mysql_connect():
     db_con = mysql.connector.connect(
         host=os.environ.get("MYSQL_HOST", "127.0.0.1"),
-        port=os.environ.get("PORT"),
-        user=os.environ.get("USER"),
-        passwd=os.environ.get("PASSWORD"),
-        database=os.environ.get("DATABASE"),
+        port=os.environ.get("MYSQL_PORT"),
+        user=os.environ.get("MYSQL_USER"),
+        passwd=os.environ.get("MYSQL_PASSWORD"),
+        database=os.environ.get("MYSQL_DATABASE"),
     )
     return db_con.is_connected()
 
