749a1e868b2054aab6924442436393cb6448633a
Anson Ming <128398768+AnsonMing@users.noreply.github.com>
2024-03-19 21:54:36 +0800

    add redis

--- code/main.py
+++ code/main.py
@@ -3,6 +3,7 @@ import json
 from flask import Flask, request, jsonify
 import mysql.connector
 import os
+import redis
 
 app = Flask(__name__)
 
@@ -11,16 +12,25 @@ def check_mysql_connect():
     db_con = mysql.connector.connect(
         host=os.environ.get("MYSQL_HOST", "127.0.0.1"),
         port=os.environ.get("MYSQL_PORT"),
-        user=os.environ.get("MYSQL_USER"),
-        passwd=os.environ.get("MYSQL_PASSWORD"),
+        user=os.environ.get("MYSQL_USER", "root"),
+        passwd=os.environ.get("MYSQL_ROOT_PASSWORD"),
         database=os.environ.get("MYSQL_DATABASE"),
     )
     return db_con.is_connected()
 
 
+def check_redis_connect():
+    r = redis.StrictRedis(host=os.environ.get("REDIS_HOST"))
+    return r.set("foo", "bar")
+
+
 @app.route("/")
 def index():
+    result = ""
     if check_mysql_connect():
-        return f"Connected Mysql!!"
-    else:
-        return f"Cannot connect Mysql!!"
+        result += "mysql connected\n"
+
+    if check_redis_connect():
+        result += "redis connected"
+
+    return result
